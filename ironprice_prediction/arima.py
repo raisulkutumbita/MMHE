c31e96931e4f78e4bdb28d4841b6efde2390daf8
raisuljs <raisul.kutumbita@gmail.com>
2019-06-19 16:28:57 +0600

    Changed date limit

--- ironprice_prediction/arima.py
+++ ironprice_prediction/arima.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import pandas as pd
 from datetime import datetime as dat
@@ -26,8 +27,20 @@ class ArimaUnivarient(APIView):
     serializer_class = ArimaSerializer
 
     def get(self, request, *args, **kwargs):
-        start_date = self.request.query_params.get('startdate', '1970-01-30')
-        end_date = self.request.query_params.get('enddate', '2018-01-01')
+        first_date = str(UnivarientData.objects.earliest('date').date)
+        last_date = str(UnivarientData.objects.latest('date').date - datetime.timedelta(days=360))
+
+        start_date = self.request.query_params.get('startdate', first_date)
+        end_date = self.request.query_params.get('enddate', last_date)
+
+        if end_date > last_date:
+            end_date = last_date
+
+        date_valid = UnivarientData.objects.exclude(date__gt=end_date).exclude(date__lt=start_date)
+
+        if not date_valid:
+            start_date = first_date
+            end_date = last_date
 
         data = read_frame(UnivarientData.objects.all())
         data['date'] = pd.to_datetime(data['date'])
@@ -40,6 +53,7 @@ class ArimaUnivarient(APIView):
         nextmonth = enddate + relativedelta.relativedelta(months=1)
 
         train, test = data[startdate:nextmonth], data[nextmonth:]
+
         arima = SARIMAX(train,order=(1, 0, 2),freq='M',seasonal_order=(1, 1, 2, 6), trend='t', enforce_stationarity=False, enforce_invertibility=False).fit()
         predict=arima.predict(test.index.min(),test.index.max())
         predictdata = pd.DataFrame(
@@ -60,13 +74,15 @@ class ArimaForecast(APIView):
     def get(self, request, *args, **kwargs):
         n_steps = int(self.request.query_params.get('nsteps', 10))
 
+        last_date = UnivarientData.objects.latest('date')
+
         data = read_frame(UnivarientData.objects.all())
         data['date'] = pd.to_datetime(data['date'])
         data = data.drop('id', axis=1)
         data = data.set_index('date')
         arima = SARIMAX(data, order=(1, 0, 2), freq='M', seasonal_order=(1, 2, 1, 6),
                         enforce_stationarity=False, enforce_invertibility=False, ).fit()
-        date_index = pd.date_range(start='1/1/2019', periods=n_steps, freq='M')
+        date_index = pd.date_range(start=last_date.date, periods=n_steps, freq='M')
         data = pd.DataFrame()
         data['prediction']  = arima.predict(date_index.min(),date_index.max())
         data['date'] = date_index
