30c5f2e77f3cf2a236be3dffe2224a85186f0b5c
surya <surya@bluescheme.ai>
2019-06-19 12:16:08 +0530

    arimaupdatedjune19-2019

--- ironprice_prediction/arima.py
+++ ironprice_prediction/arima.py
@@ -28,17 +28,18 @@ class ArimaUnivarient(APIView):
     def get(self, request, *args, **kwargs):
         start_date = self.request.query_params.get('startdate', '1970-01-30')
         end_date = self.request.query_params.get('enddate', '2018-01-01')
-
         data = read_frame(UnivarientData.objects.all())
         data['date'] = pd.to_datetime(data['date'])
         data = data.drop('id', axis=1)
         data = data.set_index('date')
-
         startdate = dat.strptime(start_date, '%Y-%m-%d')
         enddate = dat.strptime(end_date, '%Y-%m-%d')
+        maxdate=data.index.max()
+        if enddate > maxdate:
+            end_date='2018-01-01'
+            enddate = dat.strptime(end_date, '%Y-%m-%d')
 
         nextmonth = enddate + relativedelta.relativedelta(months=1)
-
         train, test = data[startdate:nextmonth], data[nextmonth:]
         arima = SARIMAX(train,order=(1, 0, 2),freq='M',seasonal_order=(1, 1, 2, 6), trend='t', enforce_stationarity=False, enforce_invertibility=False).fit()
         predict=arima.predict(test.index.min(),test.index.max())
@@ -66,7 +67,7 @@ class ArimaForecast(APIView):
         data = data.set_index('date')
         arima = SARIMAX(data, order=(1, 0, 2), freq='M', seasonal_order=(1, 2, 1, 6),
                         enforce_stationarity=False, enforce_invertibility=False, ).fit()
-        date_index = pd.date_range(start='1/1/2019', periods=n_steps, freq='M')
+        date_index = pd.date_range(start='7/1/2019', periods=n_steps, freq='M')
         data = pd.DataFrame()
         data['prediction']  = arima.predict(date_index.min(),date_index.max())
         data['date'] = date_index
