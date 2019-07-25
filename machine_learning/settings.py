4374b2d961070617d95bdb13a1122041989b65dc
raisuljs <raisul.kutumbita@gmail.com>
2019-07-25 11:24:41 +0600

    Added more port

--- machine_learning/settings.py
+++ machine_learning/settings.py
@@ -58,7 +58,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://10.100.10.20:3000",
     "http://35.154.44.20:3003",
     "http://localhost:3003",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    'http://13.127.144.175:3003/'
 ]
 
 ROOT_URLCONF = 'machine_learning.urls'
