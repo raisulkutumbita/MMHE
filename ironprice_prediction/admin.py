8e6afd33c3697c6e42eb31744964a93e067ea9a3
raisulkutumbita <raisul@kutumbita.com>
2019-08-21 16:36:39 +0600

    Updated admin

--- ironprice_prediction/admin.py
+++ ironprice_prediction/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UnivarientData, PriceProduction, CycloneData
+from .models import UnivarientData, PriceProduction, CycloneData, MultivarientData
 
 
 class UnivarientDataAdmin(admin.ModelAdmin):
@@ -11,4 +11,4 @@ admin.site.register(UnivarientData, UnivarientDataAdmin)
 # Register your models here.
 admin.site.register(PriceProduction)
 admin.site.register(CycloneData)
-
+admin.site.register(MultivarientData)
